d8108177164ff93f906b148e3f58557c727cd006
OkiljonMuhammad <maxmadmurodovoqiljon@gmail.com>
2024-07-13 17:40:56 +0500

    Masala yechildi va yangi fayl qo'shildi

---
+++ git_test.py
@@ -0,0 +1,8 @@
+with open("git_text.txt", "r") as file:
+    # file.write("1 2 3 4 5 6 7 8 9 10")
+    numbers = file.read().split(' ')
+    yigindi = 0
+    for number in numbers:
+        yigindi = yigindi + int(number)
+
+    print(yigindi)
