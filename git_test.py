f567ba5f717b5116fb34738c2e5da8410b5367c4
OkiljonMuhammad <142194331+OkiljonMuhammad@users.noreply.github.com>
2024-07-13 17:58:14 +0500

    Update git_test.py
    Add comment

--- git_test.py
+++ git_test.py
@@ -1,3 +1,6 @@
+""" “raqamlar” nomli tekst fayl ichiga 1 dan 10 gacha 
+sonlarni kiritib, ularning yig'indisini ekranga 
+chiqaruvchi dastur yarating."""
 with open("git_text.txt", "r") as file:
     # file.write("1 2 3 4 5 6 7 8 9 10")
     numbers = file.read().split(' ')
